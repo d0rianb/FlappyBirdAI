2ab6f4b73d929b86fbaca873e838b0b9b26945d0
d0rian <dorian.beauchesne@free.fr>
2020-02-02 22:48:19 +0100

    Index with spaces

--- js/env.js
+++ js/env.js
@@ -1,69 +1,69 @@
 const pipeGap = 370
 
 class Ground {
-	constructor() {
-		this.x = Infinity
-		this.y = 80
-		this.speed = 5
-		this.pipes = []
-	}
+    constructor() {
+        this.x = Infinity
+        this.y = 80
+        this.speed = 5
+        this.pipes = []
+    }
 
-	restart() {
-		this.pipes = []
-		this.speed = 5
-		this.generatePipes(10)
-	}
+    restart() {
+        this.pipes = []
+        this.speed = 5
+        this.generatePipes(10)
+    }
 
-	generatePipes(number) {
-		for (let i = 0; i < number; i++) {
-			this.pipes.push(new Pipe(window.innerWidth + i * pipeGap, Math.random() * 500 + 150, this.pipes.length))
-		}
-	}
+    generatePipes(number) {
+        for (let i = 0; i < number; i++) {
+            this.pipes.push(new Pipe(window.innerWidth + i * pipeGap, Math.random() * 500 + 150, this.pipes.length))
+        }
+    }
 
-	managePipes() {
-		this.pipes = this.pipes.filter(pipe => pipe.x + pipe.width >= 0)
-		if (this.pipes.length < 3) {
-			this.generatePipes(10)
-		}
-	}
+    managePipes() {
+        this.pipes = this.pipes.filter(pipe => pipe.x + pipe.width >= 0)
+        if (this.pipes.length < 3) {
+            this.generatePipes(10)
+        }
+    }
 
-	update() {
-		this.pipes.forEach(pipe => {
-			pipe.x -= this.speed
-		})
-		this.managePipes()
-	}
+    update() {
+        this.pipes.forEach(pipe => {
+            pipe.x -= this.speed
+        })
+        this.managePipes()
+    }
 
-	render(ctx) {
-		let groundImg = new Image()
-		groundImg.src = 'ressources/ground.png'
-		let groundTexture = ctx.createPattern(groundImg, 'repeat')
-		ctx.fillStyle = groundTexture
-		ctx.fillRect(0, window.innerHeight - this.y, window.innerWidth, this.y)
-	}
+    render(ctx) {
+        let groundImg = new Image()
+        groundImg.src = 'ressources/ground.png'
+        let groundTexture = ctx.createPattern(groundImg, 'repeat')
+        ctx.fillStyle = groundTexture
+        ctx.fillRect(0, window.innerHeight - this.y, window.innerWidth, this.y)
+    }
 }
 
 class Pipe {
-	constructor(x, y, id) {
-		this.id = id
-		this.x = x
-		this.y = y
-		this.width = 80
-		this.focus = false
-		this.gap = Math.random() * 30 + 50
-	}
+    constructor(x, y, id) {
+        this.id = id
+        this.x = x
+        this.y = y
+        this.width = 80
+        this.focus = false
+        this.gap = Math.random() * 30 + 50
+    }
 
-	render(ctx) {
-		let pipeImg = new Image()
-		pipeImg.src = 'ressources/pipe-bottom.png'
-		this.focus ? pipeImg.src = 'ressources/pipe-bottom-focus.png' : null
-		ctx.drawImage(pipeImg, this.x, 0, this.width, this.y - this.gap) // Pipe Top
-		ctx.drawImage(pipeImg, this.x, this.y + this.gap, this.width, window.innerHeight) // Pipe Bottom
+    render(ctx) {
+        let pipeImg = new Image()
+        pipeImg.src = 'ressources/pipe-bottom.png'
+        this.focus ? pipeImg.src = 'ressources/pipe-bottom-focus.png' : null
+        ctx.drawImage(pipeImg, this.x, 0, this.width, this.y - this.gap) // Pipe Top
+        ctx.drawImage(pipeImg, this.x, this.y + this.gap, this.width, window.innerHeight) // Pipe Bottom
 
-		let pipeTopImg = new Image()
-		pipeTopImg.src = 'ressources/pipe-top.png'
-		this.focus ? pipeTopImg.src = 'ressources/pipe-top-focus.png' : null
-		ctx.drawImage(pipeTopImg, this.x - 3, this.y - this.gap - 45, this.width + 6, 45)
-		ctx.drawImage(pipeTopImg, this.x - 3, this.y + this.gap, this.width + 6, 45)
-	}
+        let pipeTopImg = new Image()
+        pipeTopImg.src = 'ressources/pipe-top.png'
+        this.focus ? pipeTopImg.src = 'ressources/pipe-top-focus.png' : null
+        ctx.drawImage(pipeTopImg, this.x - 3, this.y - this.gap - 45, this.width + 6, 45)
+        ctx.drawImage(pipeTopImg, this.x - 3, this.y + this.gap, this.width + 6, 45)
+    }
 }
--- js/main.js
+++ js/main.js
@@ -30,123 +30,123 @@ let player = new Bird(birdRadius, ground, true)
 let enemy;
 
 function restartGame(mode) {
-	console.log('restart game with mode : ', mode)
-	select.value = mode
-	restartButton.style.display = 'none'
-	if (requestID !== 0) window.cancelAnimationFrame(requestID)
-	lost = false
-	brain = new Brain()
-	ground = new Ground()
-	player = new Bird(birdRadius, ground, true)
-	enemy = undefined
+    console.log('restart game with mode : ', mode)
+    select.value = mode
+    restartButton.style.display = 'none'
+    if (requestID !== 0) window.cancelAnimationFrame(requestID)
+    lost = false
+    brain = new Brain()
+    ground = new Ground()
+    player = new Bird(birdRadius, ground, true)
+    enemy = undefined
 
-	if (mode == 'auto') {
-		populationNumber = 75
-		neEV.set({
-			population: populationNumber,
-			network: [2, [2], 1],
-			historic: 2,
-			elitism: 0.3,
-			randomBehaviour: 0.27
-		})
-		brain.start()
-	} else if (mode == 'versus') {
-		enemy = new CheatedBird(birdRadius, ground);
+    if (mode == 'auto') {
+        populationNumber = 75
+        neEV.set({
+            population: populationNumber,
+            network: [2, [2], 1],
+            historic: 2,
+            elitism: 0.3,
+            randomBehaviour: 0.27
+        })
+        brain.start()
+    } else if (mode == 'versus') {
+        enemy = new CheatedBird(birdRadius, ground);
 
-	}
-	ground.generatePipes(10);
-	select.blur()
-	canvas.focus()
-	requestID = window.requestAnimationFrame(render)
+    }
+    ground.generatePipes(10);
+    select.blur()
+    canvas.focus()
+    requestID = window.requestAnimationFrame(render)
 }
 
 // Events
 ['load', 'resize'].forEach(event => {
-	window.addEventListener(event, e => {
-		canvas.width = window.innerWidth
-		canvas.height = window.innerHeight
-	})
+    window.addEventListener(event, e => {
+        canvas.width = window.innerWidth
+        canvas.height = window.innerHeight
+    })
 })
 
 select.addEventListener('change', e => {
-	MODE = e.target.value
-	restartGame(MODE)
+    MODE = e.target.value
+    restartGame(MODE)
 })
 
 restartButton.addEventListener('click', e => restartGame(MODE))
 
 window.addEventListener('keydown', key => {
-	if (key.keyCode == 32) { // Space Bar
-		player.jump()
-	}
+    if (key.keyCode == 32) { // Space Bar
+        player.jump()
+    }
 })
 
 function splashScreen() {
-	ctx.font = '2em Roboto'
-	ctx.fillStyle = '#e5e5e5'
-	ctx.fillText(`Score : ${player.passedPipe.length}`, window.innerWidth / 2 - 25, window.innerHeight / 2 - 50)
-	restartButton.style.display = 'block'
+    ctx.font = '2em Roboto'
+    ctx.fillStyle = '#e5e5e5'
+    ctx.fillText(`Score : ${player.passedPipe.length}`, window.innerWidth / 2 - 25, window.innerHeight / 2 - 50)
+    restartButton.style.display = 'block'
 }
 
 function frameRate(timestamp) {
-	let fps = Math.round(1e3 / (timestamp - previousTimestamp))
-	previousTimestamp = timestamp
-	ctx.font = '1em Roboto'
-	ctx.fillStyle = '#e5e5e5'
-	ctx.fillText(`${fps} fps`, 20, 30)
+    let fps = Math.round(1e3 / (timestamp - previousTimestamp))
+    previousTimestamp = timestamp
+    ctx.font = '1em Roboto'
+    ctx.fillStyle = '#e5e5e5'
+    ctx.fillText(`${fps} fps`, 20, 30)
 }
 
 function displayInformations() {
-	ctx.font = '1em Roboto'
-	ctx.fillStyle = '#e5e5e5'
-	ctx.fillText(`Génération n° : ${brain.generation}`, 20, 50)
-	ctx.fillText(`Alives : ${brain.alives}/${brain.gen.length}`, 20, 70)
-	ctx.fillText(`Score Max : ${brain.maxScore}`, 20, 90)
-	ctx.fillText(`Birds Best Score: ${brain.bestBird.passedPipe.length}`, 20, 110)
-	ctx.fillText(`Birds Current Score: ${brain.currentBestScore}`, 20, 130)
+    ctx.font = '1em Roboto'
+    ctx.fillStyle = '#e5e5e5'
+    ctx.fillText(`Génération n° : ${brain.generation}`, 20, 50)
+    ctx.fillText(`Alives : ${brain.alives}/${brain.gen.length}`, 20, 70)
+    ctx.fillText(`Score Max : ${brain.maxScore}`, 20, 90)
+    ctx.fillText(`Birds Best Score: ${brain.bestBird.passedPipe.length}`, 20, 110)
+    ctx.fillText(`Birds Current Score: ${brain.currentBestScore}`, 20, 130)
 }
 
 function displayScore() {
-	ctx.font = '1em Roboto'
-	ctx.fillStyle = '#e5e5e5'
-	ctx.fillText(`Score : ${player.passedPipe.length}`, 20, 50)
+    ctx.font = '1em Roboto'
+    ctx.fillStyle = '#e5e5e5'
+    ctx.fillText(`Score : ${player.passedPipe.length}`, 20, 50)
 }
 
 function update() {
-	if (lost) return
-	if (MODE == 'auto') {
-		brain.update()
-	} else if (MODE == 'player' || MODE == 'versus') {
-		player.update()
-	}
-	if (MODE == 'versus') {
-		enemy.update()
-	}
-	ground.update()
+    if (lost) return
+    if (MODE == 'auto') {
+        brain.update()
+    } else if (MODE == 'player' || MODE == 'versus') {
+        player.update()
+    }
+    if (MODE == 'versus') {
+        enemy.update()
+    }
+    ground.update()
 }
 
 function render(timestamp) {
-	ctx.clearRect(0, 0, window.innerWidth, window.innerHeight)
-	update()
-	frameRate(timestamp)
-	ground.pipes.forEach(pipe => pipe.render(ctx))
-	ground.render(ctx)
-	if (MODE == 'auto') {
-		brain.birds.forEach(bird => bird.render(ctx))
-		displayInformations()
-	} else if (MODE == 'player' || MODE == 'versus') {
-		player.render(ctx)
-		displayScore()
-	}
-	if (MODE == 'versus') {
-		enemy.render(ctx)
-	}
+    ctx.clearRect(0, 0, window.innerWidth, window.innerHeight)
+    update()
+    frameRate(timestamp)
+    ground.pipes.forEach(pipe => pipe.render(ctx))
+    ground.render(ctx)
+    if (MODE == 'auto') {
+        brain.birds.forEach(bird => bird.render(ctx))
+        displayInformations()
+    } else if (MODE == 'player' || MODE == 'versus') {
+        player.render(ctx)
+        displayScore()
+    }
+    if (MODE == 'versus') {
+        enemy.render(ctx)
+    }
 
-	if (lost) {
-		splashScreen()
-		return
-	}
-	window.requestAnimationFrame(render)
+    if (lost) {
+        splashScreen()
+        return
+    }
+    window.requestAnimationFrame(render)
 }
 
 restartGame(MODE)
